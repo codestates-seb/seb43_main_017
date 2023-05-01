08b73315075d23aa7d32d2adf0c373f90b208b15
JUBEE <119921683+mscojl24@users.noreply.github.com>
2023-05-01 23:55:42 +0900

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import "./css/reset.css"
-import NotFound from './pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <BrowserRouter>{/* 리액트 Router 연결 */}
-      <NotFound></NotFound>
       <App />
     </BrowserRouter>
   </>
