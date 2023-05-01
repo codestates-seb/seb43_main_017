ea85d4a2bd98a04170d469a475ee7abd77c73050
mscojl24 <mscojl24@naver.com>
2023-05-01 23:52:38 +0900

    feat : 404 Not found page

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./reset.css"
+import { BrowserRouter } from 'react-router-dom';
+import "./css/reset.css"
+import NotFound from './pages/NotFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
+    <BrowserRouter>{/* 리액트 Router 연결 */}
+      <NotFound></NotFound>
+      <App />
+    </BrowserRouter>
   </>
 );
 
