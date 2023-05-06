1073bdf6aa5d690432405f09b6c97d42490d124f
demuu <preasim.kr@gmail.com>
2023-05-07 03:38:40 +0900

    feat : be-feat-PlayListService#60

--- server/mainProject/src/main/java/com/codestates/mainProject/playList/service/PlayListService.java
+++ server/mainProject/src/main/java/com/codestates/mainProject/playList/service/PlayListService.java
@@ -2,7 +2,10 @@ package com.codestates.mainProject.playList.service;
 
 import com.codestates.mainProject.exception.BusinessLogicException;
 import com.codestates.mainProject.exception.ExceptionCode;
+import com.codestates.mainProject.member.entity.Member;
+import com.codestates.mainProject.music.entity.Music;
 import com.codestates.mainProject.musicLike.entity.MusicLike;
+import com.codestates.mainProject.playList.dto.PlayListDto;
 import com.codestates.mainProject.playList.entity.PlayList;
 import com.codestates.mainProject.playList.repository.PlayListRepository;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,24 @@ public class PlayListService {
     public PlayListService(PlayListRepository playListRepository) {
         this.playListRepository = playListRepository;
     }
+
+    public PlayList createPlayList(PlayListDto.PostDto postDto) {
+        PlayList playList = new PlayList();
+
+        Member member = new Member();
+        member.setMemberId(postDto.getMemberId());
+        playList.setMember(member);
+
+        Music music = new Music();
+        music.setMusicId(postDto.getMusicId());
+        playList.setMusic(music);
+
+        playList.setTitle(postDto.getTitle());
+        playList.setBody(postDto.getBody());
+
+        return playListRepository.save(playList);
+    }
+
 
     public PlayList findVerifiedPlayList(long playListId) {
         Optional<PlayList> optionalPlayList = playListRepository.findById(playListId);
