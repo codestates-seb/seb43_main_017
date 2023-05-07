c61405ce37829890323dddcc1e609f53071c2991
hunm719 <hunm719@naver.com>
2023-05-07 21:53:38 +0900

    Feat : dev-feat-뮤직서비스구현#45

--- server/mainProject/src/main/java/com/codestates/mainProject/music/repository/MusicRepository.java
+++ server/mainProject/src/main/java/com/codestates/mainProject/music/repository/MusicRepository.java
@@ -4,5 +4,8 @@ package com.codestates.mainProject.music.repository;
 import com.codestates.mainProject.music.entity.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MusicRepository extends JpaRepository<Music, Long> {
+    Optional<Music> findByMusicUri(String musicUri);
 }
--- server/mainProject/src/main/java/com/codestates/mainProject/music/service/MusicService.java
+++ server/mainProject/src/main/java/com/codestates/mainProject/music/service/MusicService.java
@@ -4,9 +4,13 @@ import com.codestates.mainProject.exception.BusinessLogicException;
 import com.codestates.mainProject.exception.ExceptionCode;
 import com.codestates.mainProject.music.entity.Music;
 import com.codestates.mainProject.music.repository.MusicRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,11 +23,63 @@ public class MusicService {
         this.musicRepository = musicRepository;
     }
 
-    public Music findVerifiedMusic(long musicId) {
+    // Music 생성(등록)
+    public Music createMusic(Music music) {
+        verifyExistMusicUri(music.getMusicUri());
+
+        // music 에 맞는 태그를 생성하거나 지정해줘야함
+
+        Music savedMusic = musicRepository.save(music);
+
+        return savedMusic;
+    }
+
+    // Music 수정
+    public Music updateMusic(Music music) {
+        Music findMusic = findVerifiedMusic(music.getMusicId());
+
+        Optional.ofNullable(music.getArtistName())
+                .ifPresent(name -> findMusic.setArtistName(name));
+        Optional.ofNullable(music.getAlbumName())
+                .ifPresent(name -> findMusic.setAlbumName(name));
+        Optional.ofNullable(music.getMusicTime())
+                .ifPresent(time -> findMusic.setMusicTime(time));
+
+        return findMusic;
+    }
+
+    // musicId 로 Music 조회
+    public Music findMusicById(long musicId) {
+        return findVerifiedMusic(musicId);
+    }
+
+    // Music 전체 조회
+    public Page<Music> findAllMusic(int page, int size) {
+        return musicRepository.findAll(PageRequest.of(page, size,
+                Sort.by("musicId").descending()));
+    }
+
+    // Music 삭제
+    public void deleteMusic(long musicId) {
+        findVerifiedMusic(musicId);
+
+        musicRepository.deleteById(musicId);
+    }
+
+    // 유효한 musicId 인지 조회
+    private Music findVerifiedMusic(long musicId) {
         Optional<Music> optionalMusic = musicRepository.findById(musicId);
         Music findMusic = optionalMusic.orElseThrow(() ->
                 new BusinessLogicException(ExceptionCode.MUSIC_NOT_FOUND));
 
         return findMusic;
+    }
+
+    // 이미 등록된 musicUri 인지 조회
+    private void verifyExistMusicUri(String musicUri) {
+        Optional<Music> optionalMusic = musicRepository.findByMusicUri(musicUri);
+        if(optionalMusic.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.MUSIC_EXISTS);
+        }
     }
 }
