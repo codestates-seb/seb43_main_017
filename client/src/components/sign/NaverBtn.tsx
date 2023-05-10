99815557a56432bdc940370b00338768a8ee1510
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-05-10 19:04:50 +0900

    feat : 네이버 Oauth 구현 중

--- client/src/components/sign/NaverBtn.tsx
+++ client/src/components/sign/NaverBtn.tsx
@@ -2,10 +2,32 @@ import { SiNaver } from 'react-icons/si';
 import { accessToken } from 'src/recoil/Atoms';
 import { useSetRecoilState } from 'recoil';
 import { OauthBtn } from './Signin';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { LoginPost } from '@/types/AxiosInterface';
+import useScript from 'src/hook/useScript';
 
+declare global {
+    interface Window {
+        naver: any;
+    }
+}
 function NaverBtn() {
-    const BaseUrl = 'https://c2fe-59-17-229-47.ngrok-free.app/members/login';
+    const { naver } = window;
     const setTokenState = useSetRecoilState(accessToken);
+    const status = useScript('https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js');
+    const BaseUrl = 'https://c2fe-59-17-229-47.ngrok-free.app/members/login';
+
+    useEffect(() => {
+        const naverLogin = new naver.LoginWithNaverId({
+            clientId: process.env.REACT_APP_CLIENT_ID,
+            callbackUrl: 'http://localhost:3000/',
+            isPopup: false,
+            loginButton: { color: 'green', type: 1, height: 30 },
+            callbackHandle: true,
+        });
+        naverLogin.init();
+    }, [status]);
 
     /**2023/05/05 - 로그인 시 서버로부터 받아 온 Access토큰을 로컬스토리지에 저장하고 로그인 모달을 종료한다 -bumpist  */
     const NaverHandler = () => {
@@ -14,10 +36,7 @@ function NaverBtn() {
 
     return (
         <>
-            <OauthBtn bgColor="#15c654" color="#fff" onClick={NaverHandler}>
-                <SiNaver className="navericon" />
-                네이버 계정으로 로그인하기
-            </OauthBtn>
+            <div id="naverIdLogin"></div>
         </>
     );
 }
