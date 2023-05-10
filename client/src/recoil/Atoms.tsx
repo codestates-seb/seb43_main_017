f6a3a7baccf62c4e8342fb4ddae59d007b01121a
hong <forhreve24@gmail.com>
2023-05-10 19:59:04 +0900

    feat : pull 수정

--- client/src/recoil/Atoms.tsx
+++ client/src/recoil/Atoms.tsx
@@ -27,8 +27,9 @@ export const selectedTagsState = atom<string[]>({
 export const musicDataState = atom<string[]>({
     key: 'musicDataState',
     default: [],
-      
-/* 2023.05.10 로그인이 발급받는 엑세스토큰 상태 관리 - 홍혜란 */
+});
+
+/* 2023.05.10 로그인이 발급받는 엑세스토큰 상태 관리 - 박수범 */
 export const accessToken = atom<string | null>({
     key: 'accessToken',
     default: '',
