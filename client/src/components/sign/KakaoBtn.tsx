18af0cc5565f402c14f7bfad24ac810f1dc7b54d
parksubeom <sooknise@naver.com>
2023-05-12 21:52:26 +0900

    feat : 카카오 Oauth 로그인 구현

--- client/src/components/sign/KakaoBtn.tsx
+++ client/src/components/sign/KakaoBtn.tsx
@@ -2,24 +2,24 @@ import { RiKakaoTalkFill } from 'react-icons/ri';
 import { accessToken } from 'src/recoil/Atoms';
 import { useSetRecoilState } from 'recoil';
 import { OauthBtn } from './Signin';
+import axios from 'axios';
 
 function KakaoBtn() {
-    const BaseUrl = 'https://c2fe-59-17-229-47.ngrok-free.app/members/login';
-    const setTokenState = useSetRecoilState(accessToken);
+    const REST_API_KEY = '86b53da526902a3a1b6004af05a90009';
+    const REDIRECT_URI = 'http://localhost:3000/oauthloading';
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-    /**2023/05/05 - 카카오 Oauth 로그인 요청 함수 -bumpist  */
-    const KakaoHandler = () => {
-        console.log('카카오로그인이다.');
-    };
-
+    const kakaoLogin = () => {
+      window.location.href = KAKAO_AUTH_URL;
+    }
     return (
-        <>
-            <OauthBtn bgColor="#fee500" color="#2e2e2e" onClick={KakaoHandler}>
-                <RiKakaoTalkFill className="kakaoicon" />
+      <>
+        <OauthBtn bgColor="#fee500" color="#2e2e2e" onClick={kakaoLogin}>
+                <RiKakaoTalkFill className="googleicon" />
                 카카오 계정으로 로그인하기
             </OauthBtn>
-        </>
-    );
-}
+      </>
+    )
+  }
 
 export default KakaoBtn;
