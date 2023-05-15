60d5ccb37ed529bf0bbec8a9e592380a15c6b584
demuu <preasim.kr@gmail.com>
2023-05-15 17:00:46 +0900

    feature/PlayListComment#142

--- server/mainProject/src/main/java/com/codestates/mainProject/playListComment/entity/PlayListComment.java
+++ server/mainProject/src/main/java/com/codestates/mainProject/playListComment/entity/PlayListComment.java
@@ -1,4 +1,31 @@
 package com.codestates.mainProject.playListComment.entity;
 
-public class PlayListComment {
+import com.codestates.mainProject.audit.Auditable;
+import com.codestates.mainProject.member.entity.Member;
+import com.codestates.mainProject.playList.entity.PlayList;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class PlayListComment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "playlist_id")
+    private PlayList playList;
 }
