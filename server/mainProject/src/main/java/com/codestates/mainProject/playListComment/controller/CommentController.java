375061a3f7a885f74bf7ccae710ef51ba1327d10
demuu <preasim.kr@gmail.com>
2023-05-15 17:31:24 +0900

    feature/PlayList-Data-Connection#154

--- server/mainProject/src/main/java/com/codestates/mainProject/playListComment/controller/CommentController.java
+++ server/mainProject/src/main/java/com/codestates/mainProject/playListComment/controller/CommentController.java
@@ -16,20 +16,21 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @Validated
+@RequestMapping("/playlist-comments")
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping("/comments")
+    @PostMapping
     public ResponseEntity<PlayListComment> createComment(@LoginMemberId Long memberId,
-                                                         @PathVariable Long playListId,
+                                                         @PathVariable("playlist-id") Long playListId,
                                                          @RequestBody String content) {
         PlayListComment comment = commentService.createComment(memberId, playListId, content);
         return new ResponseEntity<>(comment, HttpStatus.CREATED);
     }
 
     //{playlist-id} 댓글 전체 조회
-    @GetMapping("/{playlist-id}")
-    public ResponseEntity<List<PlayListComment>> getCommentsByPlayListId(@PathVariable Long playListId){
+    @GetMapping("/{comment-id}")
+    public ResponseEntity<List<PlayListComment>> getCommentsByPlayListId(@PathVariable("playlist-id") Long playListId){
         List<PlayListComment> comments = commentService.getCommentsByPlaylistId(playListId);
         return new ResponseEntity<>(comments, HttpStatus.OK);
     }
@@ -37,16 +38,16 @@ public class CommentController {
     // 수정
     @PatchMapping("/{comment-id}")
     public ResponseEntity<PlayListComment> updateComment(@LoginMemberId Long memberId,
-                                                         @PathVariable Long commentId,
+                                                         @PathVariable("comment-id") Long commentId,
                                                          @RequestBody String content) {
         PlayListComment updatedComment = commentService.updateComment(memberId, commentId, content);
         return new ResponseEntity<>(updatedComment, HttpStatus.OK);
     }
 
     //삭제
-    @DeleteMapping("{comment-id")
+    @DeleteMapping("/{comment-id}")
     public ResponseEntity<Void> deleteComment(@LoginMemberId Long memberId,
-                                              @PathVariable Long commentId) {
+                                              @PathVariable("comment-id") Long commentId) {
         commentService.deleteComment(memberId, commentId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
