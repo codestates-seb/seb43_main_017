0689405c6ff14d53d43d50f220feae1a566bccbf
demuu <preasim.kr@gmail.com>
2023-05-18 04:29:39 +0900

    be-feat-tags#183

--- server/mainProject/src/main/java/com/codestates/mainProject/playList/service/PlayListService.java
+++ server/mainProject/src/main/java/com/codestates/mainProject/playList/service/PlayListService.java
@@ -3,16 +3,13 @@ package com.codestates.mainProject.playList.service;
 import com.codestates.mainProject.exception.BusinessLogicException;
 import com.codestates.mainProject.exception.ExceptionCode;
 import com.codestates.mainProject.member.entity.Member;
+import com.codestates.mainProject.member.repository.MemberRepository;
 import com.codestates.mainProject.member.service.MemberService;
-import com.codestates.mainProject.music.dto.MusicDto;
 import com.codestates.mainProject.music.entity.Music;
 import com.codestates.mainProject.music.service.MusicService;
-import com.codestates.mainProject.playList.dto.PlayListDto;
 import com.codestates.mainProject.playList.entity.PlayList;
 import com.codestates.mainProject.playList.repository.PlayListRepository;
-import com.codestates.mainProject.playlListMusic.entity.PlayListMusic;
-import com.codestates.mainProject.playlListMusic.service.PlayListMusicService;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,7 +17,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,19 +26,27 @@ import org.slf4j.LoggerFactory;
 @Slf4j
 @Service
 @Transactional
-@RequiredArgsConstructor
 public class PlayListService {
     private static final Logger logger = LoggerFactory.getLogger(PlayListService.class);
 
     private final PlayListRepository playListRepository;
+    private final MemberRepository memberRepository;
     private final MemberService memberService;
-    private final PlayListMusicService playListMusicService;
+    private final MusicService musicService;
+
+    public PlayListService(PlayListRepository playListRepository,
+                           MemberRepository memberRepository, MemberService memberService, MusicService musicService) {
+        this.playListRepository = playListRepository;
+        this.memberRepository = memberRepository;
+        this.memberService = memberService;
+        this.musicService = musicService;
+    }
 
     public PlayList createPlayList(PlayList playList) {
         try {
             long memberId = playList.getMember().getMemberId();
             Member findMember = memberService.findVerifiedMember(memberId);
-            playList.setCreateMember(findMember.getName());
+            playList.setCreate(findMember.getName());
 
             findMember.addPlayList(playList);
 
@@ -67,70 +71,47 @@ public class PlayListService {
         //TODO: 각 맴버가 가진 플레이리스트 조회기능 만들기
     }
 
-    public PlayList updatePlayList(Long playListId, Long memberId, PlayListDto.PatchDto requestBody){
-        PlayList findPlayList = findVerifiedPlayList(playListId);
-        Member findMember = memberService.findMember(memberId);
-
-        if (!findPlayList.getMember().getMemberId().equals(findMember.getMemberId())) {
-            throw new BusinessLogicException(ExceptionCode.NO_PERMISSION_EDITING_COMMENT);
-        } else {
-            if (requestBody.getTitle() != null) {
-                findPlayList.setTitle(requestBody.getTitle());
-            }
-            if (requestBody.getBody() != null) {
-                findPlayList.setBody(requestBody.getBody());
-            }
-        }
-
-        return findPlayList;
+    public PlayList updatePlayList(PlayList playList){
+        PlayList findPlayList = findVerifiedPlayList(playList.getPlayListId());
+        Optional.ofNullable(playList.getTitle()).ifPresent(findPlayList::setTitle);
+        Optional.ofNullable(playList.getBody()).ifPresent(findPlayList::setBody);
+        return playListRepository.save(findPlayList);
     }
 
-    public void deletePlayList(long playListId, long memberId){
-        PlayList playList = findPlayList(playListId);
-        Member member = memberService.findMember(memberId);
-
-        if (!member.getMemberId().equals(playList.getMember().getMemberId()) || !member.getRoles().contains("ADMIN")) {
-            throw new BusinessLogicException(ExceptionCode.NO_PERMISSION_EDITING_COMMENT);
-        }
-        playListRepository.delete(playList);
+    public void deletePlayList(long playListId, long currentUserId){
+        //TODO: 어드민, 유저 구분해서 삭제 기능
+        PlayList findPlayList = findVerifiedPlayList(playListId);
+        playListRepository.delete(findPlayList);
     }
 
     // 플리 안에 있는 음악 하나 삭제
     public void deleteMusicFromPlayList(long playListId, long musicId){
-
-        PlayListMusic playListMusic = playListMusicService.findVerifiedPlayListMusic(playListId, musicId);
-        Music findMusic = playListMusic.getMusic();
-        PlayList findPlayList = playListMusic.getPlayList();
-
+        PlayList playList = findVerifiedPlayList(playListId);
 
         //TODO: 인증된 사용자 정보 가져오기
 
-        List<Music> musics = findPlayList.getMusics();
+        List<Music> musics = playList.getMusics();
         // 음악 찾고 삭제
         Optional<Music> optionalMusic = musics.stream()
                 .filter(music -> music.getMusicId().equals(musicId))
                 .findFirst();
         if (optionalMusic.isPresent()) {
-
-            findMusic.removePlayListMusic(playListMusic);
-            findPlayList.removePlayListMusic(playListMusic);
-            playListMusicService.deleteMemberMusic(playListId,musicId);
-
+            Music musicToRemove = optionalMusic.get();
+            musics.remove(musicToRemove);
+            playListRepository.save(playList);
         } else throw new BusinessLogicException(ExceptionCode.MUSIC_NOT_FOUND);
     }
 
     // 만들어진 플리에 음악 추가
-    public void addMusicToPlayList(long playListId, long musicId) {
-        PlayListMusic playListMusic = playListMusicService.createPlayListMusic(this, playListId, musicId);
+    public void addMusicToPlayList(long playListId, long musicId){
+        PlayList playList = findVerifiedPlayList(playListId);
+        Music music = musicService.findMusicById(musicId);
 
+        List<Music> musics = playList.getMusics();
+        musics.add(music);
 
-        PlayList playList = playListMusic.getPlayList();
-        Music music = playListMusic.getMusic();
-
-        playList.addPlayListMusic(playListMusic);
-        music.addPlayListMusic(playListMusic);
-        }
-
+        playListRepository.save(playList);
+    }
 
     public PlayList findVerifiedPlayList(long playListId) {
         Optional<PlayList> optionalPlayList = playListRepository.findById(playListId);
@@ -141,30 +122,9 @@ public class PlayListService {
     }
 
     // 플리 안에 있는 음악 조회
-    public List<MusicDto.ResponseDto> findVerifiedPlayListMusic(long playListId) {
+    public List<Music> findVerifiedPlayListMusic(long playListId) {
         PlayList playList = findVerifiedPlayList(playListId);
-        List<Music> musics = playList.getMusics();
-        List<MusicDto.ResponseDto> musicDtos = new ArrayList<>();
-
-        for (Music music : musics) {
-            MusicDto.ResponseDto musicDto = new MusicDto.ResponseDto(
-                    music.getMusicId(),
-                    music.getMusicName(),
-                    music.getArtistName(),
-                    music.getAlbumName(),
-                    music.getMusicTime(),
-                    music.getAlbumCoverImg(),
-                    music.getMusicUri(),
-                    music.getCreatedAt().toString(),
-                    music.getModifiedAt().toString(),
-                    new ArrayList<>(music.getTags()),
-                    playList.getMember().getMemberId()
-
-            );
-
-            musicDtos.add(musicDto);
-        }
-
-        return musicDtos;
+        return playList.getMusics();
     }
+
 }
