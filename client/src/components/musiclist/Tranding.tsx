9bd0ea5a887f0d9ee5399de0aa09fe2ed6d8a885
mscojl24 <mscojl24@naver.com>
2023-05-20 21:18:02 +0900

    콘솔삭제

--- client/src/components/musiclist/Tranding.tsx
+++ client/src/components/musiclist/Tranding.tsx
@@ -29,7 +29,6 @@ const Trending = () => {
             })
             .then((response) => {
                 setTranding(response.data.data);
-                console.log(response.data.data);
             })
             .catch((error) => {
                 console.error(error);
