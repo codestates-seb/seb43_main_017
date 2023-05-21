106c54a95695c04017b9f6de3f523d86fa8278ea
mscojl24 <mscojl24@naver.com>
2023-05-21 21:09:29 +0900

    feat : 디테일 페이지 음원재생 기능 추가 (초기)

--- client/src/components/soundbar/SoundBar.tsx
+++ client/src/components/soundbar/SoundBar.tsx
@@ -1,10 +1,29 @@
 import styled from 'styled-components';
+import React from 'react';
+import ReactPlayer from 'react-player';
 
 function SoundBar() {
-    return <Soundbarsection></Soundbarsection>;
+    return (
+        <Soundbarsection>
+            <ReactPlayer
+                style={playerStyle}
+                url="https://mainproject-uncover.s3.ap-northeast-2.amazonaws.com/music/600Horses-Cinco.mp3"
+                controls
+                playing
+                volume={0.5}
+                width="640  px"
+                height="360px"
+            />
+        </Soundbarsection>
+    );
 }
 
 export default SoundBar;
+
+const playerStyle = {
+    border: '2px solid #ccc',
+    borderRadius: '8px',
+};
 
 const Soundbarsection = styled.div`
     position: fixed;
@@ -14,7 +33,7 @@ const Soundbarsection = styled.div`
     background-color: rgba(20, 20, 20, 1);
     z-index: 3;
     width: 800px;
-    height: 60px;
+    height: 500px;
     border-radius: 30px 30px 0px 0px;
     animation: showbar 1s forwards;
 
