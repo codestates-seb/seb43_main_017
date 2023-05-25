d1d55365326cceaf1585218f9bbc77034bb6af5f
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-05-25 17:35:53 +0900

    refactor : 불필요한 임포트 삭제 및 jsdocs 추가

--- client/src/components/mixing/VideoPlayer.tsx
+++ client/src/components/mixing/VideoPlayer.tsx
@@ -1,9 +1,9 @@
-import React, { LegacyRef } from 'react';
+import React, { Ref } from 'react';
 import styled from 'styled-components';
 /** 2022/05/22 - Video 정보 타입 선언 - 박수범*/
 interface VideoPlayerProps {
     videoUrl: string;
-    videoRef: LegacyRef<HTMLVideoElement>;
+    videoRef: Ref<HTMLVideoElement>;
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl, videoRef }) => {
