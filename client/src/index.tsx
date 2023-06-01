fc2918fbdadd0f427ffc2ebe2488fe2af3a5a724
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-06-01 23:32:42 +0900

    fix : 이전버전으로 리셋

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import 'src/css/index.css';
 import App from 'src/App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <BrowserRouter>
-        <RecoilRoot>
-            <App />
-        </RecoilRoot>
-    </BrowserRouter>,
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>,
 );
