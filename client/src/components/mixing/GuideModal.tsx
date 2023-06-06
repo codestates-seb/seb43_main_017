96fe2d5c6598ea12235b93cc5c3ab8d3ca3a8a50
Bumpist <104641096+parksubeom@users.noreply.github.com>
2023-06-06 17:54:45 +0900

    feat : 피팅룸 페이지 가이드 영상 밑 안내문구 추가

--- client/src/components/mixing/GuideModal.tsx
+++ client/src/components/mixing/GuideModal.tsx
@@ -7,8 +7,9 @@ const GuideModal = () => {
     const setGuideModal = useSetRecoilState(GuideModalState);
     return (
         <ModalContainer>
+            <GuideGif src="./assets/guide.gif" alt="guidegif" />
             <p>1.영상을 업로드한다.</p>
-            <p>2.촤측에 좋아요한 음원 리스트에서 + 버튼을 눌러 음원을 선택한다.</p>
+            <p>2.좌측에 좋아요한 음원 리스트에서 + 버튼을 눌러 음원을 선택한다.</p>
             <p>3.영상 아래 컨트롤바를 통해 재생/정지/음원 사운드를 조절한다.</p>
             <p>4.내 영상과 음원의 핏이 맞는지 확인한다.</p>
             <p>5.마음에 들었다면 음원을 다운받으러 간다.</p>
@@ -34,6 +35,27 @@ const ModalContainer = styled.div`
     backdrop-filter: blur(6px);
     -webkit-backdrop-filter: blur(6px);
     border: 1px solid rgba(255, 255, 255, 0.18);
+    > p {
+        margin: 10px 10px;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    @media screen and (max-width: 700px) {
+        > p {
+            margin: 10px 10px;
+            font-size: 14px;
+            font-weight: 500;
+        }
+    }
+`;
+const GuideGif = styled.img`
+    width: 600px;
+    height: 350px;
+    margin-bottom: 30px;
+    @media screen and (max-width: 800px) {
+        width: 350px;
+        height: 180px;
+    }
 `;
 
 const Exitbox = styled.div`
